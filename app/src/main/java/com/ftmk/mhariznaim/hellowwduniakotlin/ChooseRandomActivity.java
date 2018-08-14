f3df54406717cc9748f514f393234734a1a54a2a
mhariznaim <mhariznaim@gmai.com>
2018-08-14 16:44:53 +0800

    Add menu and Web View

--- app/src/main/java/com/ftmk/mhariznaim/hellowwduniakotlin/ChooseRandomActivity.java
+++ app/src/main/java/com/ftmk/mhariznaim/hellowwduniakotlin/ChooseRandomActivity.java
@@ -1,8 +1,13 @@
 package com.ftmk.mhariznaim.hellowwduniakotlin;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +17,8 @@ public class ChooseRandomActivity extends AppCompatActivity {
 
     EditText edtGuess;
     int intCnt = 0, intRandom;
-    Button btnGuess,btnReset;
+    Button btnGuess, btnReset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,43 +27,33 @@ public class ChooseRandomActivity extends AppCompatActivity {
         btnGuess = findViewById(R.id.btnGuess);
         btnReset = findViewById(R.id.btnReset);
 
-        intRandom =(int) (Math.random() * 100);
-        Log.d("NomborTeka" , intRandom+"");
+        intRandom = (int) (Math.random() * 100);
+        Log.d("NomborTeka", intRandom + "");
 
-        if(intCnt < 3)
-        {
+        if (intCnt < 3) {
             btnReset.setVisibility(View.INVISIBLE);
         }
     }
 
     public void fnGuess(View view) {
-        Log.d("NomborTeka" , intRandom+"");
-        if(intCnt < 3)
-        {
+        Log.d("NomborTeka", intRandom + "");
+        if (intCnt < 3) {
 
             int intUserNum = Integer.valueOf(edtGuess.getText().toString());
-            if(intUserNum == intRandom)
-            {
-                Toast.makeText(getApplicationContext(),"Fuyoo betul teka", Toast.LENGTH_LONG).show();
-            }
-            else if( intUserNum < intRandom)
-            {
-                Toast.makeText(getApplicationContext(),"Kurang sangat tu nombor", Toast.LENGTH_LONG).show();
+            if (intUserNum == intRandom) {
+                Toast.makeText(getApplicationContext(), "Fuyoo betul teka", Toast.LENGTH_LONG).show();
+            } else if (intUserNum < intRandom) {
+                Toast.makeText(getApplicationContext(), "Kurang sangat tu nombor", Toast.LENGTH_LONG).show();
+                intCnt++;
+            } else if (intUserNum > intRandom) {
+                Toast.makeText(getApplicationContext(), "Besauu sangat tu nombor", Toast.LENGTH_LONG).show();
                 intCnt++;
             }
-            else if( intUserNum > intRandom)
-            {
-                Toast.makeText(getApplicationContext(),"Besauu sangat tu nombor", Toast.LENGTH_LONG).show();
-                intCnt++;
-            }
-        }
-        else
-        {
+        } else {
             btnReset.setVisibility(View.VISIBLE);
             btnGuess.setVisibility(View.INVISIBLE);
-            Toast.makeText(getApplicationContext(),"Dah Teka Lebih 3 la bang", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Dah Teka Lebih 3 la bang", Toast.LENGTH_LONG).show();
         }
-
 
 
     }
@@ -66,5 +62,55 @@ public class ChooseRandomActivity extends AppCompatActivity {
         btnGuess.setVisibility(View.VISIBLE);
         btnReset.setVisibility(View.INVISIBLE);
         intCnt = 0;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_pilih,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.menuAboutUs:
+                //go to new Page ; Web Activity/ Web View.
+                Intent intent = new Intent(ChooseRandomActivity.this, WebActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.menuContactUs:
+                //display Toast message
+                Toast.makeText(getApplicationContext(),"You can contact us at 014444 tekan tak dapat",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menuShare:
+                //share by Dialog
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ChooseRandomActivity.this);
+                alertDialogBuilder.setTitle("Nak Kongsi kepada Masyarakat?");
+                alertDialogBuilder.setMessage("Betul ni nak kongsi kepada masyakat?");
+                alertDialogBuilder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alertDialogBuilder.setNegativeButton("NO!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alertDialogBuilder.show();
+                break;
+
+            case R.id.menuExit:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/ftmk/mhariznaim/hellowwduniakotlin/WebActivity.java
@@ -0,0 +1,29 @@
+package com.ftmk.mhariznaim.hellowwduniakotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity extends AppCompatActivity {
+
+    WebView webViewUs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        webViewUs = findViewById(R.id.webView1);
+
+        webViewUs.loadUrl("http://ftmk.utem.edu.my/web/index.php/about/staff-directory/staff-directory-department-of-software-engineering/");
+        webViewUs.getSettings().setJavaScriptEnabled(true);
+        webViewUs.setWebViewClient(new WebViewClient(){
+            @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        webViewUs.setWebChromeClient(new WebChromeClient());
+    }
+}
